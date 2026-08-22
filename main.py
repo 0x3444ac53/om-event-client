d16ff802e645cb3c86cf14214376a58d7f14c0a9
Nora <elliemaegalyean@gmail.com>
2026-08-22 14:09:01 -0400

    okay so I removed all the debugging statements, and also made it so it should auto detect your save directory. Is system agnostic, I think

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 import base64
-from xxlimited import Str
 import httpx
 import json
 import questionary
 import sys
-from pprint import pp
+from platform import system
 from pathlib import Path
 
+
 base_url = "https://events.critelli.technology"
-puzzle_goes_here = Path("/Users/Nora/Library/Application Support/Opus Magnum/76561198361444248/custom/")
+
+match system(): # System Specific Save Dir
+    case "Windows":
+        puzzle_goes_here = Path.home() / Path("Documents/My Games/Opus Magnum")
+    case "Darwin":
+        puzzle_goes_here = Path.home() / Path("Library/Application Support/Opus Magnum")
+    case "Linux":
+        puzzle_goes_here = Path.home() / Path(".local/share/Opus Magnum")
+    case _:
+        raise RuntimeError("The fuck is your OS???")
+
+# find NUMBERS
+puzzle_goes_here /= [i for i in puzzle_goes_here.iterdir() if i.name.isdigit()][0] / "custom"
+
 
 def main():
     timeframe = is_oki(base_url)
     time = questionary.select("View Collections?", choices=timeframe.keys()).ask()
     collections = {i["collectionName"] : i for i in timeframe[time]}
-    pp(collections)
     collection = collections[
             questionary.select(">",
                                choices=collections.keys()).ask()
             ]
-    print(collection)
     events = {i["title"] : i for i in collection['events']}
-    pp(events)
     event = events[questionary.select(collection["collectionName"], choices=events.keys()).ask()]
     puzzle = is_oki(event["url"])
     with open(puzzle_goes_here / puzzle['puzzleFileName'], 'wb') as f:
